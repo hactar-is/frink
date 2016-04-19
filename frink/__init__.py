d90a03521d869dcb6fc6ce48d33e3cf1a5cd4549
Andy Beaumont <andy@andybeaumont.com>
2016-04-19 12:09:32 +0100

    Try to unify the version number.

---
+++ frink/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Frink
+    ~~~~~~~~~~~~~
+    Frink is a kind of ORM thing for RethinkDB built on top of the excellent Schematics.
+"""
+
+__version__ = "0.0.7.dev"
--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
+import ast
 import setuptools
 
+with file('frink/__init__.py') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = ast.parse(line).body[0].value.s
+            break
 
-def get_requirements(suffix=''):
-    with open('requirements%s.txt' % suffix) as f:
-        rv = f.read().splitlines()
-    return rv
-
+assert version is not None
 
 setuptools.setup(
     name="frink",
-    version="0.0.3",
+    version=version,
     url="https://github.com/hactar-is/frink",
 
     author="Hactar",
@@ -21,7 +23,13 @@ setuptools.setup(
 
     packages=setuptools.find_packages(),
 
-    install_requires=get_requirements(),
+    install_requires=[
+        'flask',
+        'schematics',
+        'rethinkdb==2.3.0',
+        'flask-security',
+        'inflection'
+    ],
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
